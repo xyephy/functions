9f153415c75a89d83c34d6eb7f98c3cebca71c18
xyephy <ziphyr.xyeag@gmail.com>
2022-07-01 06:16:17 +0300

    Adding multiple parameters
    an example of how to add multiple parameters to a function.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    another_function(5);
+    print_labeled_measurement(5, 'h');
 }
 
-// adding parameters
-fn another_function(x: i32) {
-    println!("The value of x is: {}", x);
+// adding multiple parameters
+fn print_labeled_measurement(value: 5, unit_label: char) {
+    println!("The measurement is: {}{}", value, unit_label);
 }
